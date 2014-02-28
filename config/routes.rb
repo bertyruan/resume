25ced15aa6a79d7fef1eadff3e5ff5148a81ba81
bertyruan <berty.ruan@gmail.com>
2014-02-28 15:22:11 -0500

    More Updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Resume::Application.routes.draw do
   root "static_page#home"
+
+  match '/#education', to: 'static_pages#home', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
